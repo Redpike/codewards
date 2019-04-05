e00362e25d05ae610b8e0d4324cad6ee94c13592
Redpike <rsokulski@op.pl>
2019-04-05 22:12:13 +0200

    4th challenge done in TypeScript

---
+++ TypeScript/d004/d004.ts
@@ -0,0 +1,5 @@
+export function removeChar(str: string): string {
+  return str.slice(1, -1);
+}
+
+console.log(removeChar('wers'));
---
+++ TypeScript/out/d004/d004.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function removeChar(str) {
+    return str.slice(1, -1);
+}
+exports.removeChar = removeChar;
+console.log(removeChar('wers'));
+//# sourceMappingURL=d004.js.map
