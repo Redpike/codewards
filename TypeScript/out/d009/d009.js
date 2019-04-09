5f4dc8b1ddeaffcecac55d0c91c95725f1363b5e
Redpike <rsokulski@op.pl>
2019-04-09 18:12:02 +0200

    9th challenge in TypeScript done

---
+++ TypeScript/out/d009/d009.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hexToDec(hexString) {
+    return parseInt(hexString, 16);
+}
+exports.hexToDec = hexToDec;
+//# sourceMappingURL=d009.js.map
